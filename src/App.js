a7842446c59ab57322b170327cf3095b311f59e9
Nick <n.t.1996@mail.ru>
2025-05-08 12:54:20 +0400

    work in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+// декларативный стиль
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// императивный стиль
 export const App = () => {
+	const year = new Date().getFullYear();
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		// декларативный стиль
+		createElement(
+			'div',
+			{ className: 'App' },
+			createElement(
+				'header',
+				{ className: 'App-header' },
+				createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+				createElement(
+					'p',
+					null,
+					'Edit',
+					createElement('code', null, 'src/App.js'),
+					' and save to reload.',
+				),
+				createElement(
+					'a',
+					{
+						className: 'App-link',
+						href: 'https://reactjs.org',
+						target: '_blank',
+						rel: 'noopener noreferrer',
+					},
+					'Learn React',
+				),
+				createElement('div', null, year),
+			),
+		)
 	);
 };
